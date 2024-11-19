5712b35c1116d480f82af31b59f4b4214976debe
mika <303014439@qq.com>
2024-11-19 11:08:00 +0800

    Submit description

--- testcase/__init__.py
+++ testcase/__init__.py
@@ -1,10 +1,2 @@
 # -*- coding : UTF-8 -*-
-"""
--------------------------------------------
- @Company    : 上海米哈游天命科技有限公司
- @Project    : mihoyo-srmqatest
- @Author     : yulong.guo
- @Datetime   : 2024/11/19 10:24
- @Filename   : __init__.py.py
- ------------------------------------------
-"""
+
